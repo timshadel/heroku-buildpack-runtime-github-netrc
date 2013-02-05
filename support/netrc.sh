b64d5fcac3f7b106801dd1acc91b29d3b55121d1
Tim Shadel <github@timshadel.com>
2013-02-04 17:32:03 -0700

    Fix file creation setup.

--- support/netrc.sh
+++ support/netrc.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-echo /dev/null > ~/.netrc
+cat /dev/null > ~/.netrc
 chmod 600 ~/.netrc
 echo "machine github.com login $GITHUB_AUTH_TOKEN password x-oauth-basic" >> ~/.netrc
 unset GITHUB_AUTH_TOKEN
 
-echo /dev/null > ~/.curlrc
+cat /dev/null > ~/.curlrc
 chmod 600 ~/.curlrc
 echo "--netrc-optional" >> ~/.curlrc
 
